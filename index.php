fbd10846f29595e7aff83f57d0e61e497cd72656
steveharwell1 <steveharwell@gmail.com>
2019-08-25 20:51:04 +0000

    Commiting pre-existing files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+  include 'templates/header.php';
+  
+  $name = 'Remind Me';
+  echo "This is the $name App. This is PHP!";
+
+  include 'templates/footer.php'
+?>
+
---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Remind Me!
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+
+    <body>
