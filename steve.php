fecf5382ba38305df25b0d9201ee43a8b5cf602b
steveharwell1 <steveharwell@gmail.com>
2019-09-03 21:41:13 +0000

    Steve's info page added

---
+++ steve.php
@@ -0,0 +1,12 @@
+<?php 
+  include_once 'db/test_db.php';
+  include 'utils/debug.php';
+  include 'templates/header.php';
+
+  $name = 'Steve';
+  echo "This is the $name App. This is PHP!";
+
+
+  include 'templates/footer.php'
+?>
+
