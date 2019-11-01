0d0e0513c21e86c8015c22005776573f8bc614ba
steveharwell1 <steveharwell@gmail.com>
2019-11-01 15:38:47 +0000

    added CategoryView.php

--- templates/CategoryView.php
+++ templates/CategoryView.php
@@ -1,14 +1,15 @@
 <h2>CATEGORY VIEW</h2>
+
 <ul>
   <li>G1: Category Name</li>
     <ul>
+      <li>Job 0</li>
       <li>Job 1</li>
-      <li>Job 2</li>
     </ul>
   <li>G2: Category Name</li>
       <ul>
-      <li>Job 1</li>
       <li>Job 2</li>
+      <li>Job 3</li>
     </ul>
 </ul>
 
