e7890fa2710ac5ed29a1ad7c57e95242ab496ae2
dthinglama <52077676+dthinglama@users.noreply.github.com>
2019-11-08 22:39:09 -0600

    Update groupView.php

--- templates/groupView.php
+++ templates/groupView.php
@@ -1,1 +1,13 @@
-<h2>My Groups</h2>
+<h2>MY GROUPS</h2>
+<ul>
+  <li>Group Name </li>
+	<ul>Members
+	<li>Member 1</li>
+	<li>Member 2</li>
+	</ul>
+ 
+  <li>Owned Groups</li>
+  
+  <button>Create new group</button>  <button>Join a group</button>
+  
+</ul>
