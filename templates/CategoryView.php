5aea01b1070c27b41750e06d60a2d5196afc6fb2
steveharwell1 <steveharwell@gmail.com>
2019-11-09 20:13:46 +0000

    added CategoryView

--- templates/CategoryView.php
+++ templates/CategoryView.php
@@ -12,34 +12,3 @@
     </ul>
 </ul>
 
-<script>
-CategoryView = document.getElementById('addGroup');
-
-var addJobReq = new XMLHttpRequest();
-function addGroupCallback() {
-	text = this.responseText;
-  //Transform text into a native data sctructure
-  	message = JSON.parse(text);
-	  console.log(text);
-	document.getElementById('groupsList').innerText += message.data.name;
-}
-//This attaches a function to oReq that will handle responses from the server.
-addJobReq.addEventListener("load", addGroupCallback);
-
-
-//This is the event that sends the message to the server.
-CategoryView.addEventListener('click', function(){
-    console.log('sending message');
-    addJobReq.open("POST", "/templates/groupController.php");
-    name = document.getElementById('addGroupName');
-
-
-    //setting the header so that JSON will be sent. Both directions of communication will be json.
-    //It is the common language of this informal network layer. REST runs like this. Most things called
-    //REST are really just json communication. So if you get this then you get like 70% of what people
-    //mean when they say REST.
-    addJobReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    numUsers = JSON.stringify({ "name": name.value });
-    addJobReq.send(numUsers);
-});
-</script>
