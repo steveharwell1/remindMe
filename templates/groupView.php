bfaf8a6fd33e15b94542f249bb1df0841b219783
dthinglama <52077676+dthinglama@users.noreply.github.com>
2019-11-11 23:52:24 -0600

    Update groupView.php

--- templates/groupView.php
+++ templates/groupView.php
@@ -1,13 +1,49 @@
-<h2>MY GROUPS</h2>
-<ul id="groupsList">
-  <li>Group Name </li>
-	<ul>Members
-	<li>Member 1</li>
-	<li>Member 2</li>
-	</ul>
- 
-  <li>Owned Groups</li>
-  
-  <button>Create new group</button>  <button>Join a group</button>
-  
-</ul>
+<?php
+include("test_db.php");
+error_reporting(0);
+?>
+
+<html>
+<head> 
+
+</head>
+
+<body>
+	<form action ="" method="GET">
+	Group ID 	<input type="text" name="GroupID" value=""/><br><br>
+	Group name	<input type="text" name="GroupName" value=""/><br><br>
+	Group owner	<input type="text" name="GroupOwner" value=""/><br><br>
+	Super group	<input type="text" name="SuperGroup" value=""/><br><br>
+	<input type="submit" name="submit" value="Create"/>
+	</form>
+
+<?php
+if($_GET['submit'])
+{
+	$groupID = $_GET['GroupID'];
+	$groupName= $_GET['GroupName'];
+	$groupOwner= $_GET['GroupOwner`'];
+	$superGroup= $_GET['SuperGroup'];
+	
+	if($groupID!="" && $groupName!="" && $groupOwner!="" && $superGroup!="")
+	{
+		$query= "INSERT INTO GROUPS VALUES(`$groupID`, `$groupName`,
+				`$groupOwner`, `$superGroup`)";
+		$data= mysqli_query($db,$query);
+		
+		if($data)
+		{
+			echo "Data inserted into Database"
+		}
+	}
+	else
+	{
+		echo "All fields are required";
+	}
+}
+
+
+?>
+</body>
+
+</html>
