db787448398d12957405ff1e9f35954956755ef3
dthinglama <52077676+dthinglama@users.noreply.github.com>
2019-11-12 00:37:42 -0600

    Update groupView.php

--- templates/groupView.php
+++ templates/groupView.php
@@ -14,3 +14,31 @@
 </body>
 
 </html>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title>DELETE GROUP</title>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+
+    <body>
+
+        <form action="deleteGroup.php" method="post">
+
+            Enter Group ID to delete:&nbsp;<input type="text" name="GROUP_ID" required><br><br>
+
+            <input type="submit" name="delete" value="Delete Group">
+
+        </form>
+
+    </body>
+
+</html>
