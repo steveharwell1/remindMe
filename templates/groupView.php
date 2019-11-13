be0d1ada76ed3f9bb505986479ce782863ef2b9d
dthinglama <52077676+dthinglama@users.noreply.github.com>
2019-11-13 16:17:37 -0600

    Update groupView.php

--- templates/groupView.php
+++ templates/groupView.php
@@ -6,13 +6,13 @@
 
 ?>
 
-	<form action ="" method="GET">
+<form action ="" method="GET">
 	Group ID 	<input type="text" name="GroupID" value=""/><br><br>
 	Group name	<input type="text" name="GroupName" value=""/><br><br>
 	Group owner	<input type="text" name="GroupOwner" value=""/><br><br>
 	Super group	<input type="text" name="SuperGroup" value=""/><br><br>
 	<input type="submit" name="submit" value="Create"/>
-	</form>
+</form>
 
 <?php
 if($_GET['submit'])
