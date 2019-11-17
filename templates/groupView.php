f99eabfc038ba862e55c145b95f626799da7bcbd
dthinglama <dheerazthing.lama@gmail.com>
2019-11-17 22:54:55 +0000

    Updating local repo and pushing changes

--- templates/groupView.php
+++ templates/groupView.php
@@ -1,13 +1,47 @@
-<h2>MY GROUPS</h2>
-<ul id="groupsList">
-  <li>Group Name </li>
-	<ul>Members
-	<li>Member 1</li>
-	<li>Member 2</li>
-	</ul>
- 
-  <li>Owned Groups</li>
-  
-  <button>Create new group</button>  <button>Join a group</button>
-  
-</ul>
+<?php
+ include_once '../utils/login_required.php';
+ include_once '../db/test_db.php';
+ include '../utils/debug.php';
+ include 'header.php';
+
+?>
+
+<form action ="" method="GET">
+	Group ID 	<input type="text" name="GroupID" value=""/><br><br>
+	Group name	<input type="text" name="GroupName" value=""/><br><br>
+	Group owner	<input type="text" name="GroupOwner" value=""/><br><br>
+	Super group	<input type="text" name="SuperGroup" value=""/><br><br>
+	<input type="submit" name="submit" value="submit"/>
+</form>
+
+<?php
+if($_GET['submit'])
+{
+	$groupID = mysqli_real_escape_string($db, $_GET['GroupID']);
+	$groupName= $_GET['GroupName'];
+	$groupOwner= $_GET['GroupOwner'];
+	$superGroup= $_GET['SuperGroup'];
+	
+	if(!empty($_GET['GroupID']) && !empty($_GET['GroupName']) && $groupOwner!="" && $superGroup!="")
+	{
+		$query= "INSERT INTO GROUPS VALUES('$groupID', '$groupName',
+				'$groupOwner', '$superGroup')";
+		$data= mysqli_query($db,$query);
+		
+		if($data)
+		{
+			echo "Data inserted into Database";
+		} else {
+
+		}
+	}
+	else
+	{
+		echo "All fields are required";
+	}
+}
+
+?>
+<?php
+include '/templates/footer.php';
+?>
